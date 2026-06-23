588dcc81f69cb63ce473eff35cdb15f2fec86244
Tsuji <geekjumpingcarnival@i.softbank.jp>
2026-06-24 08:05:57 +0900

    feat: add shop and purchases routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get "profile", to: "profile#show", as: :profile
+  resources :shop, only: [ :index ] do
+    resources :purchases, only: [ :create ]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
